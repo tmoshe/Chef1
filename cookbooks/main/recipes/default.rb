26b917481040e47e70218f77698f7f558556c238
tmoshe <64310013+tmoshe@users.noreply.github.com>
2023-06-24 18:34:29 +0300

    Update default.rb

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -46,9 +46,9 @@ execute "run-flask-apk"  do
 
 end
 
-remote_file_s3 '/home/ubuntu/app/my-app-19.tar' do
+remote_file_s3 '/home/ubuntu/app/my-app-21.tar' do
   bucket 'cloudschoolartifacts'
-  remote_path 'my-app-19.tar'
+  remote_path 'my-app-21.tar'
   aws_access_key_id 'AKIASF5WYYCAZFLVH755'
   aws_secret_access_key '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'
   region 'eu-west-1'
